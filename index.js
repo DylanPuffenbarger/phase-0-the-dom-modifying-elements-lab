a13e4d7ecad8b297ea4d5f056c8ebad3885a1821
Dylan Puffenbarger <dylanpuffenbarger@gmail.com>
2023-07-29 04:05:57 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+// Removes <main> elements w/ id 'main'
+document.getElementById('main').remove();
+
+// Creates new header
+const body = document.getElementsByTagName('body')[0];
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 
+    "Dylan is the champion";
+body.append(newHeader);
